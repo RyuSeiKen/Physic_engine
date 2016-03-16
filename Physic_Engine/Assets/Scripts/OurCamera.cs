9f975f88f2d095be7199d12dc1cfa30d8e081e1b
James Petit <jp.james.petit@gmail.com>
2016-03-16 17:52:54 +0100

    Camera enabled.

---
+++ Physic_Engine/Assets/Scripts/OurCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OurCamera : MonoBehaviour 
+{
+	void Update () 
+	{
+		float mouseX = Input.GetAxis ("Mouse X");
+		float mouseY = Input.GetAxis ("Mouse Y");
+		transform.Translate (mouseX, mouseY, 0.0f);
+	}
+}
