060fd222ac00c15491fb6fe8cb5cceca1fdf7f8b
James Petit <jp.james.petit@gmail.com>
2016-03-16 18:12:51 +0100

    Better Camera.

--- Physic_Engine/Assets/Scripts/OurCamera.cs
+++ Physic_Engine/Assets/Scripts/OurCamera.cs
@@ -3,10 +3,29 @@ using System.Collections;
 
 public class OurCamera : MonoBehaviour 
 {
+
+	Vector3 target = new Vector3();
+	public float moveFactor = 10.0f;
+	public float smoothRatio = 0.9f;
+
+	void Start()
+	{
+		target = new Vector3();
+		target.z = transform.localPosition.z;
+	}
+
 	void Update () 
 	{
 		float mouseX = Input.GetAxis ("Mouse X");
 		float mouseY = Input.GetAxis ("Mouse Y");
-		transform.Translate (mouseX, mouseY, 0.0f);
+
+		float dm = Time.deltaTime * moveFactor;
+		target.x += mouseX * dm;
+		target.y += mouseY * dm;
+
+		Vector3 position = transform.localPosition;
+		Vector3 newPosition = Vector3.Lerp (target, position, smoothRatio);
+		transform.localPosition = newPosition;
+//		transform.Translate (mouseX, mouseY, 0.0f);
 	}
 }
