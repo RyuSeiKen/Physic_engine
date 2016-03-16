e18331715b8b49e84580040dd6291d4a3aca3904
James Petit <jp.james.petit@gmail.com>
2016-03-16 19:57:27 +0100

    Physics Included.

---
+++ Physic_Engine/Assets/Scripts/Spring.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spring : MonoBehaviour 
+{
+	float mass = 0.2f;
+	public float stiffness = 0.1f;
+	public Vector3 speed = new Vector3();
+
+	void Update () 
+	{
+		Vector3 position = transform.localPosition;
+		Vector3 forces = -stiffness * position - speed * 0.1f;
+
+		Vector3 acceleration = forces / mass;
+
+		speed += acceleration * Time.deltaTime;
+
+		position += speed * Time.deltaTime;
+		transform.localPosition = position;
+	}
+}
